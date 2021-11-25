5346332b33f189cb4ccb5e74c11535e91a70c703
Vibhor Meharia <vibhormeharia@gmail.com>
2021-11-25 10:15:04 +0530

    added postgres vnet rule for postgres to aks communication

--- virtual_network.tf
+++ virtual_network.tf
@@ -13,3 +13,12 @@ resource "azurerm_subnet" "snet" {
   resource_group_name  = azurerm_resource_group.rg.name
   address_prefixes     = [var.subnet_prefix]
 }
+
+# Create virtual network rule for postgres
+resource "azurerm_postgresql_virtual_network_rule" "pg_vnet_rule" {
+  name                                 = "pg-vnet-rule"
+  resource_group_name                  = azurerm_resource_group.rg.name
+  server_name                          = azurerm_postgresql_server.pg_server.name
+  subnet_id                            = azurerm_subnet.snet.id
+  ignore_missing_vnet_service_endpoint = true
+}
